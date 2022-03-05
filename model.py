aed220764e24930c5f95b3a7978d45a2136ac2cb
Harshal Hirpara <77119525+HJHirpara@users.noreply.github.com>
2022-03-05 10:50:02 +0530

    Harshal -- model.py

--- model.py
+++ model.py
@@ -326,12 +326,14 @@ def prediction_w2v(query_sentence, dataset, model, topk=5):
     return best_index
 
 def recommend(query_sentence):
+  query_sentence = query_sentence.lower()
   best_index =prediction_w2v(query_sentence, df_cleaned['All'].values, w2v_model)    
   df = df_cleaned[['Title','Authors', 'Year', 'Link']].iloc[best_index].reset_index(drop=True)
   df.index = df.index + 1
   return df
 
 def more(query_sentence, n=0):
+    query_sentence = query_sentence.lower()
     best_index =prediction_w2v(query_sentence, df_cleaned['All'].values, w2v_model , topk = 5*n)    
     df = df_cleaned[['Title','Authors', 'Year', 'Link']].iloc[best_index].reset_index(drop=True)
     df = df.iloc[5*n:5*n+1]
